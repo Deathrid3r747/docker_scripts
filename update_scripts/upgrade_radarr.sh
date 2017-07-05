731e40283122dec65b13f4140f9e9f6ddd5bb980
root <root@media.neildutoi>
2017-07-05 10:39:59 +0200

    Add scripts

---
+++ update_scripts/upgrade_radarr.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker pull linuxserver/radarr
+
+if docker ps | grep -q "linuxserver/radarr"; then
+  echo "Already running latest image"
+else
+  echo "New image pulled"
+  echo "Upgrading container"
+  docker stop radarr
+  docker rm radarr
+  docker create --name=radarr --restart=always -v /home/Containers/Radarr:/config -v /home/Media:/media -e PGID=0 -e PUID=0 -e TZ=Africa/Johannesburg --privileged -p 7878:7878 linuxserver/radarr  
+  docker start radarr
+fi
