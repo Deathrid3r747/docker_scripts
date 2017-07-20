bf8edfa13fd9a24626ca786d3022f1fe82d60155
Deathrid3r747 <neil@snail-mail.co.za>
2017-07-20 10:49:04 +0200

    Update Plex Script

--- plex.sh
+++ plex.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
 
-docker pull linuxserver/plex
+docker pull plexinc/pms-docker
 
-if docker ps | grep -q "linuxserver/plex"; then
+if docker ps | grep -q "plexinc/pms-docker"; then
   echo "Already running latest image"
 else
   echo "New image pulled"
   echo "Upgrading container"
   docker stop plex
   docker rm plex
-  docker create \
-	--name=plex \
-	--net=host \
-	-e VERSION=latest \
-	-e PUID=0 \
-	-e PGID=0 \
-	-e TZ=Africa/Johannesburg \
-	-v /home/Containers/Plex:/config \
-	-v /home/Media/Series:/data/tvshows \
-	-v /home/Media/Movies:/data/movies \
-	-v /home/Media/Transcode:/transcode \
-	linuxserver/plex
+ 	docker run \
+	 -d \
+	 --name plex \
+	 --privileged \
+	 --restart=always \
+	 -p 32400:32400/tcp \
+	 -p 3005:3005/tcp \
+	 -p 8324:8324/tcp \
+	 -p 32469:32469/tcp \
+	 -p 1900:1900/udp \
+	 -p 32410:32410/udp \
+	 -p 32412:32412/udp \
+	 -p 32413:32413/udp \
+	 -p 32414:32414/udp \
+	 -e TZ="<timezone>" \
+	 -e PLEX_CLAIM="<claimToken>" \
+	 -h <HOSTNAME> \
+	 -v <path/to/plex/database>:/config \
+	 -v <path/to/transcode/temp>:/transcode \
+	 -v <path/to/media>:/data \
+	 plexinc/pms-docker
   docker start plex
 fi
 
